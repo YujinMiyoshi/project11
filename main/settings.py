1b96e261a8cfb6aba1ea8c0463bfff9f44998986
eugene <eugene.earth.solar@gmail.com>
2022-07-18 15:53:21 +0900

    deploy

--- main/settings.py
+++ main/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&@04r9ti6aw9ji^pbsnv0jf1vt=3qpmu!6mw$k5!xoe^s*#c0'
+# SECRET_KEY = 'django-insecure-)&@04r9ti6aw9ji^pbsnv0jf1vt=3qpmu!6mw$k5!xoe^s*#c0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 
 # Password validation
